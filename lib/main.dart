905a654da68c071e9a1595b2147539b816cb349c
laithabuaisheh1 <laithabuaisheh4@gmail.com>
2025-02-04 12:07:54 +0300

    task3 04022025 12:07

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'tasks/task1.dart';
+import 'tasks/task3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Task1(),
+      home: Task3(),
     );
   }
 }
