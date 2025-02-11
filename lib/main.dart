e7a5939a63c9759633d9ddc2c5a8b4d5bbd3eb01
laithabuaisheh1 <laithabuaisheh4@gmail.com>
2025-02-11 14:34:27 +0300

    Task13 11022025 02:34

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-import 'tasks/task1.dart';
+// import 'tasks/task1.dart';
+// import 'tasks/task2.dart';
+import 'providers/login_provider.dart';
+import 'providers/product_provider.dart';
+import 'tasks/task11.dart';
 import 'tasks/task5.dart';
+import 'tasks/task6.dart';
+import 'tasks/task7.dart';
 import 'tasks/task8.dart';
-//import 'tasks/task3.dart';
+// ignore: depend_on_referenced_packages
+import 'package:provider/provider.dart';
+
+import 'views/product_view_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProductProvider()),
+        ChangeNotifierProvider(create: (context) => LoginProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,25 +35,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Task8(),
+      // this is task selector
+      //home: const Task1(),
+      //home: const Task2(),
+      home: ProductViewWidget(),
     );
   }
 }
