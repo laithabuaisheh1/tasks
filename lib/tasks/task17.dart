6c6cd41f3d8570ec790a129c98641af77d1df575
laithabuaisheh1 <laithabuaisheh4@gmail.com>
2025-02-27 11:50:48 +0300

    task17part2

--- lib/tasks/task17.dart
+++ lib/tasks/task17.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import "package:firebase_core/firebase_core.dart";
 
-class Task17 extends StatefulWidget {
-  const Task17({super.key});
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
 
   @override
-  State<Task17> createState() => _Task17State();
+  State<AuthScreen> createState() => _AuthScreenState();
 }
 
-class _Task17State extends State<Task17> {
+class _AuthScreenState extends State<AuthScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  Future<void> _task17() async {
+  String? userId;
+
+  Future<void> signUp() async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      setState(() => userId = userCredential.user?.uid);
+      await FirebaseFirestore.instance.collection("users ").doc(userId).set({
+        "email ": emailController.text,
+        "createdAt ": DateTime.now(),
+      });
+    } catch (e) {
+      print(" Signup Error : $e");
+    }
+  }
+
+  Future<void> signIn() async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
-      print(" Logged ␣in␣as:␣${userCredential.user?.email}");
-    } on FirebaseAuthException catch (e) {
-      print(" Task17 ␣ error :␣${e.message}");
+      setState(() => userId = userCredential.user?.uid);
+    } catch (e) {
+      print(" Login Error : $e");
     }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    setState(() => userId = null);
   }
 
   @override
